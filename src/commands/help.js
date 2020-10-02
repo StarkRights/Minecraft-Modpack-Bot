0d0603f81eebb6928cdb1b82fd6fc3c4a31c464d
Chase Crayne <Chawork1234@gmail.com>
2020-10-01 21:52:57 -0500

    Changed mpHelp to reflect packswith/modsin funcionality using IDS over names

--- src/commands/help.js
+++ src/commands/help.js
@@ -15,8 +15,8 @@ module.exports = {
       .addField('mp!Mods', 'Displays mods by popularity ranking.\n \`Useage: mp!mods\`', true)
       .addField('mp!Packsearch', 'Displays list of packs matching a search query.\n \`Useage: mp!packsearch <pack name>\`', true)
       .addField('mp!Modsearch', 'Displays list of mods matching a search query.\n \`Useage: mp!modsearch <mod name>\`', true)
-      .addField('mp!PacksWith', 'Displays list of packs containing a specified mod (chosen from a menu).\n \`Useage: mp!packswith <mod name>\`', true)
-      .addField('mp!ModsIn', 'Displays list of mods in a certain pack. \n \`Useage: mp!ModsIn <pack name>\`', true)
+      .addField('mp!PacksWith', 'Displays list of packs containing a specified mod (mod IDs can be found from modsearch).\n \`Useage: mp!packswith <mod ID>\`', true)
+      .addField('mp!ModsIn', 'Displays list of mods in a certain pack. (pack IDs can be found from packsearch) \n \`Useage: mp!ModsIn <pack ID>\`', true)
       .setFooter('Powered by ModPackIndex.com');
     message.channel.send(helpEmbed);
   }
